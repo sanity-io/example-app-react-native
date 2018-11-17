7436fdfbf6deaaaa2687ede78c4d5e59c1276b01
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-11-17 23:33:54 +0100

    Modernize this project with the latest versions of things With the exception of @sanity/client which needs to be fixed first

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StackNavigator} from 'react-navigation'
+import {createAppContainer, createStackNavigator} from 'react-navigation'
 
 import HomeScreen from './screens/Home'
 import MoviesScreen from './screens/Movies'
@@ -7,10 +7,12 @@ import MovieScreen from './screens/Movie'
 import PersonScreen from './screens/Person'
 import PeopleScreen from './screens/People'
 
-export default StackNavigator({
+const AppNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
   Movies: {screen: MoviesScreen},
   People: {screen: PeopleScreen},
   Movie: {screen: MovieScreen},
   Person: {screen: PersonScreen}
 })
+
+export default createAppContainer(AppNavigator)
